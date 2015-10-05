253cdcb4202476b656c58c3f0be05af997ab2be1
Melanie Sedda <sedda@csc.kth.se>
2015-10-05 14:20:55 +0200

    Resolution of the optimization problem

--- codes/svm.py
+++ codes/svm.py
@@ -1,14 +1,21 @@
 __author__ = 'melaniesedda'
 import numpy as np
 import pylab
-from generate_data import *
+import random
 from kernels import *
-#from cvxopt.solvers import qp
-#from cvxopt.base import matrix
+from cvxopt.solvers import qp
+from cvxopt.base import matrix
 
 def svm(kernel_function, args, C):
     # Generate data
-    data = generate_random_data()
+    classA = [(random.normalvariate(-1.5, 1), random.normalvariate(0.5, 1), 1.0) for i in range(5)] + \
+             [(random.normalvariate(1.5, 1), random.normalvariate(0.5, 1), 1.0) for i in range(5)]
+    classB = [(random.normalvariate(0.0, 0.5), random.normalvariate(-0.5, 0.5), -1.0) for i in range(10)]
+    data = classA + classB
+    random.shuffle(data)
+    pylab.hold(True)
+    pylab.plot([p[0] for p in classA], [p[1] for p in classA], 'bo')
+    pylab.plot([p[0] for p in classB], [p[1] for p in classB], 'ro')
     N = np.shape(data)[0]
     print N
 
@@ -23,11 +30,12 @@ def svm(kernel_function, args, C):
     alpha = list(r['x'])
 
     # Plot the boundary
-    xrange = numpy.arange(-4, 4, 0.05)
-    yrange = numpy.arange(-4, 4, 0.05)
-    grid = matrix([[indicator(x, y) for y in yrange] for x in xrange])
+    xrange = np.arange(-4, 4, 0.05)
+    yrange = np.arange(-4, 4, 0.05)
+    grid = matrix([[indicator([x, y], alpha, data, kernel_function, args) for y in yrange] for x in xrange])
     pylab.contour(xrange, yrange, grid, (-1.0, 0.0, 1.0), colors=('red', 'black', 'blue'), linewidths=(1, 3, 1))
+    pylab.show()
 
 
 if __name__ == "__main__":
-    svm(linear_kernel, [0], 0)
+    svm(rbf_kernel, [2, 3], 0)
